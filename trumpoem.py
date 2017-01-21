d253745e5530fdd90d7d97669b2cd7a719459de5
Tayler Geiger <tayler@woltergeiger.com>
2017-01-21 10:35:16 -0600

    first version of script done

---
+++ trumpoem.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from sys import argv
+from random import randint
+import string
+
+
+# organize command line arguments
+num_args = len(argv)
+poem_words = int(argv[1])
+
+# open, split, and lowercase the transcript
+text = open("transcript.txt").read().lower().split()
+# remove punctuation from edges of each word
+text = [word.strip(string.punctuation) for word in text]
+# should be 1450 words
+word_count = len(text)
+
+poem_list = []
+
+for word in range(poem_words):
+    poem_list.append(text[randint(0, word_count)])
+
+print(" ".join(poem_list))
