731559da9e66556db94bc5b5b2f7b0f2f1720780
Tayler Geiger <tayler@woltergeiger.com>
2017-01-25 12:44:35 -0600

    expanded grab_tweets to grab max tweets possible

--- twitbot.py
+++ twitbot.py
@@ -5,7 +5,7 @@ import twittercalls
 
 # create Markov text model
 text_model = markovify.Text(
-    " ".join(twittercalls.grab_tweets('realDonaldTrump', 200)))
+    " ".join(twittercalls.grab_tweets('realDonaldTrump')))
 
 # create our new tweet and post it
 tweet = text_model.make_short_sentence(140)
